c6b63b9f1ab4ad1bace1ed0cabc8d1ee18739a2c
Thomas Plaçais <thomas.placais@protonmail.com>
2025-07-25 20:31:17 +0200

    feat: display account informations

--- src/models/transaction.ts
+++ src/models/transaction.ts
@@ -6,4 +6,8 @@ export const TransactionSchema = z.object({
   id: z.int(),
   description: z.string().optional(),
   amount: z.number(),
+  sourceAccountId: z.int().optional(),
+  sourceAccountName: z.string().optional(),
+  destinationAccountId: z.int().optional(),
+  destinationAccountName: z.string().optional(),
 });
